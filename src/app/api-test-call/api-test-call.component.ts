9b9e5fdba6dde241718f8acc6775c1ea421fd5e3
Student <example@example.com>
2019-02-05 16:03:15 -0800

    creates game object

--- src/app/api-test-call/api-test-call.component.ts
+++ src/app/api-test-call/api-test-call.component.ts
@@ -6,6 +6,7 @@ import { ApiTestCallService } from '../api-test-call.service';
 import { routing } from '../app.routing';
 import * as $ from 'jquery';
 import * as dot from 'dot-wild';
+import { Game } from '../models/game.model'
 
 
 @Component({
@@ -13,15 +14,25 @@ import * as dot from 'dot-wild';
   templateUrl: './api-test-call.component.html',
   styleUrls: ['./api-test-call.component.css'],
   providers: [ApiTestCallService],
-  moduleId: module.id,
+
 
 })
 export class ApiTestCallComponent implements OnInit {
   article: any[] = null;
 
-  getArticle(query) {
-    this.wikiApiCall.getByPageId(query).subscribe(response => {
+  game: Game = new Game();
+
+  start: string = "";
+  end: string = "";
+
+  startGame(beginArticle,beginId,endArticle,endId) {
+    this.wikiApiCall.getByPageId(beginArticle).subscribe(response => {
       this.article = response.json();
+      this.game.beginArticle = beginArticle;
+      this.game.beginId = beginId;
+      this.game.endArticle = endArticle;
+      this.game.endId = endId;
+      console.log(this.game);
       $("#inputThing").val('');
       let thing = response.json().parse;
       let content = thing.text['*'];
@@ -39,27 +50,59 @@ export class ApiTestCallComponent implements OnInit {
         $("#inputThing").val(clickedLink);
         that.getArticle(clickedLink);
       })
-      $("a").dblclick(function() {
-        let clickedURL = ($(this).attr("title"));
-        let clickedLink = clickedURL.substr(clickedURL.lastIndexOf('/') + 1);
+    });
+  }
 
+  getArticle(query) {
+    this.wikiApiCall.getByPageId(query).subscribe(response => {
+      this.article = response.json();
+      console.log('the game after clicking start', this.game)
+      $("#inputThing").val('');
+      let thing = response.json().parse;
+      let content = thing.text['*'];
+      $("#output").empty();
+      $("#output").html(thing.text['*']);
+      $("a").click(function() {
         return false;
       })
-
+      let that = this;
+      $("a").click(function(event) {
+        event.preventDefault();
+        let clickedURL = ($(this).attr("href"));
+        let clickedLink = clickedURL.substr(clickedURL.lastIndexOf('/') + 1);
+        $(".checkDiv").text(clickedLink);
+        $("#inputThing").val(clickedLink);
+        that.getArticle(clickedLink);
+      })
     });
   }
 
   constructor(private wikiApiCall: ApiTestCallService, private http: Http) {
-    $.get(`https://en.wikipedia.org/w/api.php?format=json&action=query&generator=random&origin=*&grnnamespace=0&prop=revisions&rvprop=content&grnlimit=1`).then(function(response) {
-      console.log(response);
-      const thing2 = dot.get(response, 'query.pages.*.title')
-      let thing = thing2[0];
-      console.log(thing)
-      $("#inputThing").val(thing);
 
-  })
   }
 
-    ngOnInit() {  }
+    ngOnInit() {
+      $.get(`https://en.wikipedia.org/w/api.php?format=json&action=query&generator=random&origin=*&grnnamespace=0&prop=revisions&rvprop=content&grnlimit=1`).then(function(response) {
+        const thingTitle = dot.get(response, 'query.pages.*.title')
+        const thingId = dot.get(response, 'query.pages.*.pageid')
+        let start_thing_title = thingTitle[0];
+        let start_thing_id = thingId[0];
+        console.log('start article', start_thing_title, start_thing_id)
+        this.start = start_thing_id;
+        $("#beginArticle").val(start_thing_title);
+        $("#beginId").val(start_thing_id);
+
+        $.get(`https://en.wikipedia.org/w/api.php?format=json&action=query&generator=random&origin=*&grnnamespace=0&prop=revisions&rvprop=content&grnlimit=1`).then(function(response) {
+          const end_thingTitle = dot.get(response, 'query.pages.*.title')
+          const end_thingId = dot.get(response, 'query.pages.*.pageid')
+          let end_thing_title = end_thingTitle[0];
+          let end_thing_id = end_thingId[0];
+          console.log('end article', end_thing_title, end_thing_id)
+          this.end = end_thing_id;
+          $("#endArticle").val(end_thing_title);
+          $("#endId").val(end_thing_id);
+        })
+      })
+    }
 
 }
--- src/app/models/game.model.ts
+++ src/app/models/game.model.ts
@@ -1,11 +1,10 @@
 export class Game {
-  pages: string[] = [];
-  username: string = "";
-  timestamp: string = "";
+  public beginArticle: string = '';
+  public endArticle: string = '';
+  public beginId: number = null;
+  public endId: number = null;
+  public articles: string[] = [];
+  public timestamp: string = '';
 
-
-
-  constructor(public params) {
-
-  }
+  constructor() { }
 }

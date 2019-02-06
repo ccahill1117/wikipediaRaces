ae3f2e5e2ead61e3c22ab6ab08c1af6710753ca1
QuinnMiller <example@example.com>
2019-02-06 08:38:43 -0800

    add endArticle and startArticle properties to the game model

--- src/app/models/game.model.ts
+++ src/app/models/game.model.ts
@@ -2,7 +2,13 @@
 
 export class Game {
 
-  constructor( public username: string, public score: number, public history: string[] = []) {
-
+  constructor(
+    public username: string = '',
+    public score: number = null, 
+    public history: string[] = [],
+    public beginArticle: string = '',
+    public endArticle: string = '',
+    public beginId: number = null,
+    public endId: number = null) {
   }
 }
